b4be7f34b067ac284d663db0cdc6cac43a7ddd54
Jack Westbrook <jack.westbrook@gmail.com>
2023-09-21 10:51:11 +0200

    refactor: rename regex const add string and comment regexs

--- packages/systemjs-cjs-extra/src/constants.js
+++ packages/systemjs-cjs-extra/src/constants.js
@@ -5,6 +5,7 @@ export const AMD_MODULE_REGEX =
 export const CJS_EXPORTS_REGEX =
   /(?:^\uFEFF?|[^$_a-zA-Z\xA0-\uFFFF.])(exports\s*(\[['"]|\.)|module(\.exports|\['exports'\]|\["exports"\])\s*(\[['"]|[=,\.]))/;
 export const CJS_FILEDIR_REGEX = /__filename|__dirname]/;
-
-export const javascriptRegEx = /^[^#?]+\.js([?#].*)?$/;
-export const jsContentTypeRegEx = /^(text|application)\/(x-)?javascript(;|$)/;
+export const COMMENT_REGEX = /(^|[^\\])(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
+export const STRING_REGEX = /("[^"\\\n\r]*(\\.[^"\\\n\r]*)*"|'[^'\\\n\r]*(\\.[^'\\\n\r]*)*')/g;
+export const JAVASCRIPT_REGEX = /^[^#?]+\.js([?#].*)?$/;
+export const JS_CONTENT_TYPE_REGEX = /^(text|application)\/(x-)?javascript(;|$)/;
